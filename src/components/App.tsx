6d9e0e13271602cf3870af9abb00aa0c1818c605
JaeYeopHan <ljyhanll@gmail.com>
2020-02-08 19:59:04 +0900

    feat: Apply PopupError component

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,14 +3,18 @@ import { Route, Switch } from 'react-router-dom'
 
 import Main from './main/Main'
 import Post from './post/Post'
+import { PopupError } from './shared/error/PopupError'
 import { User } from './user/User'
 
 export default () => {
   return (
-    <Switch>
-      <Route path="/user/:id" component={User} />
-      <Route path="/:id" component={Post} />
-      <Route path="/" component={Main} />
-    </Switch>
+    <>
+      <Switch>
+        <Route path="/user/:id" component={User} />
+        <Route path="/:id" component={Post} />
+        <Route path="/" component={Main} />
+      </Switch>
+      <PopupError />
+    </>
   )
 }
--- src/components/shared/error/PopupError.tsx
+++ src/components/shared/error/PopupError.tsx
@@ -1,3 +1,4 @@
+import styled from '@emotion/styled'
 import React from 'react'
 import { useSelector } from 'react-redux'
 
@@ -7,20 +8,42 @@ import {
   ERROR_CODE,
   IErrorState,
 } from '@/features/common/error/ErrorSlice'
+import { colors } from '@/styles/colors'
+
+import { Dimmed } from '../layout/Dimmed'
+
+const PopupWrapper = styled.div`
+  margin: 0 auto;
+  padding: 24px;
+  width: 100%;
+  max-width: 320px;
+  height: 5em;
+  display: inline-block;
+  border-radius: 16px;
+  background-color: ${colors.white};
+  z-index: 100;
+  position: relative;
+  vertical-align: middle;
+`
+
+const PopupTitle = styled.h3`
+  font-size: 24px;
+  font-weight: bold;
+`
+
+const PopupMessage = styled.div`
+  color: ${colors.gray[4]};
+`
 
 export const PopupError = () => {
   const { code } = useSelector<IRootState, IErrorState>(state => state[ERROR])
 
-  if (code === ERROR_CODE.CLEAR) {
-    return <></>
-  }
-
   return (
-    <div>
-      <div>Error!</div>
-      <div>
-        Code: <span>{code}</span>
-      </div>
-    </div>
+    <Dimmed isShow={code === ERROR_CODE.CLEAR}>
+      <PopupWrapper>
+        <PopupTitle>Error!</PopupTitle>
+        <PopupMessage>(Code: {code})</PopupMessage>
+      </PopupWrapper>
+    </Dimmed>
   )
 }
