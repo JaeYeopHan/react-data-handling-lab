d199d9d0cbfca19f525009c6e540466c5dcdb610
JaeYeopHan <ljyhanll@gmail.com>
2020-02-08 19:58:55 +0900

    feat: Add Dimmed component

---
+++ src/components/shared/layout/Dimmed.tsx
@@ -0,0 +1,34 @@
+import styled from '@emotion/styled'
+import React, { ReactNode } from 'react'
+
+interface IDimmedProps {
+  isShow: boolean
+  children: ReactNode
+}
+
+const StyledDiv = styled<'div', IDimmedProps>('div')`
+  display: ${props => (props.isShow ? 'flex' : 'none')};
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+
+  z-index: 99;
+  width: 100%;
+  align-items: center;
+
+  &:before {
+    position: fixed;
+    top: 0;
+    right: 0;
+    bottom: 0;
+    left: 0;
+    background-color: rgba(0, 0, 0, 0.4);
+    content: '';
+  }
+`
+
+export const Dimmed = (props: IDimmedProps) => {
+  return <StyledDiv {...props}>{props.children}</StyledDiv>
+}
