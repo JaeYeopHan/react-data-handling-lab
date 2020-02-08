5929d90ff8cae0e7d19d298c8df815e36f3c8842
JaeYeopHan <ljyhanll@gmail.com>
2020-02-08 17:13:58 +0900

    feat: Add PopupError component

---
+++ src/components/shared/error/PopupError.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+interface IPopupErrorProps {}
+
+export const PopupError = (props: IPopupErrorProps) => {
+  return (
+    <div>
+      <div>Error!</div>
+    </div>
+  )
+}
