3a62d4b79812a35eb74ab8abcc803ec4ee1dc707
JaeYeopHan <ljyhanll@gmail.com>
2020-02-12 11:52:15 +0900

    feat: Apply changed decorator to stories

--- src/stories/main/Label.stories.tsx
+++ src/stories/main/Label.stories.tsx
@@ -1,18 +1,31 @@
-import React, { ReactNode } from 'react'
-import { Provider } from 'react-redux'
-import StoryRouter from 'storybook-react-router'
+import React from 'react'
 
 import { Label } from '@/components/main/Label'
 import { LabelIndex } from '@/components/main/LabelIndex'
-import store from '@/features'
+import { POST } from '@/features/post/PostSlice'
+
+import { withRedux } from '../../../.storybook/decorators/addon-redux-toolkit'
 
 export default {
   title: 'Main Page/Label',
-  decorators: [
-    (storyFn: () => ReactNode) => <Provider store={store}>{storyFn()}</Provider>,
-    StoryRouter(),
-  ],
 }
 
 export const Index = () => <LabelIndex />
 export const Item = () => <Label id={'post2'} />
+
+Item.story = {
+  decorators: [withRedux({
+    [POST]: {
+      posts: {
+        'post2': {
+          id: 'post2',
+          title: 'Mock Title',
+          author: 'Mock Author',
+          body: 'Mock body',
+          comments: [1, 2],
+        },
+      },
+      ids: ['post2'],
+    },
+  })],
+}
--- src/stories/main/Main.stories.tsx
+++ src/stories/main/Main.stories.tsx
@@ -1,18 +1,10 @@
-
-import React, { ReactNode } from 'react'
-import { Provider } from 'react-redux'
-import StoryRouter from 'storybook-react-router'
+import React from 'react'
 
 import Main from '@/components/main/Main'
-import store from '@/features'
 
 export default {
   title: 'Main Page',
   component: Main,
-  decorators: [
-    (storyFn: () => ReactNode) => <Provider store={store}>{storyFn()}</Provider>,
-    StoryRouter(),
-  ],
 }
 
 export const main = () => <Main />
